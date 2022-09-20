e436fcf03ec7424a521c13a5caec5b75b071fe51
git Ozoano <git obiuchenna74@gmail.com>
2022-09-20 11:55:28 -0700

    python loop

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print(f"{i:02d}", end=", ")
-print(f"{i + 1}")
+for i in range(100):
+    print("{:d}".format(i).rjust(2, "0"),
+          end="\n" if i == 99 else ", ")
