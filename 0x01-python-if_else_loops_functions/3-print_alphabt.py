aa7860ed3f747be116c288ca98beed54f8a958d7
git Ozoano <git obiuchenna74@gmail.com>
2022-09-20 10:52:42 -0700

    python loop

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for a in range(ord('a'), ord('z') + 1):
-    if chr(a) != 'e' and chr(a) != 'q':
-        print("{:c}".format(a), end='')
+for i in range(ord("a"), ord("z")+1):
+    if i == ord("q") or i == ord("e"):
+        continue
+
+    print("{:c}".format(i), end="")
+
