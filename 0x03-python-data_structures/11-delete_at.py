387789e9cd76932a5230e54d06757dca2e6fa4ad
git Ozoano <git obiuchenna74@gmail.com>
2022-09-23 14:35:31 -0700

    delete index

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+
+    '''
+    deletes item at a specific position
+    Args:
+        my_list - list to search
+        idx - position tk access
+    Returns:
+        my_list - if idx is out of range
+    '''
+    index = len(my_list)
+    val = my_list[idx]
+
+    if idx < 0 or idx >= index:
+        return my_list
+    my_list.remove(val)
+    return my_list
