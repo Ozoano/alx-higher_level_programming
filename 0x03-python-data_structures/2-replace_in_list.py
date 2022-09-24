64349c196fc43d139540cb6b821a61779de55999
git Ozoano <git obiuchenna74@gmail.com>
2022-09-24 03:46:45 -0700

    list replace

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list):
+    """
+    replace an element at a specific position
+    Args:
+        my_list - original list
+        idx - the index to be replaced at
+        element - the new value
+    """
+    if idx < 0 or idx >= len(my_list):
         returny_list
     my_list[idx] = element
     return my_list
