33d2505bf7041a45631265e1a86e9a873bc9f8bf
git Ozoano <git obiuchenna74@gmail.com>
2022-09-24 03:43:05 -0700

    list copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    """
+    replace an element at a position without
+    modifying the original list
+    Args:
+        my_list - the original list
+        idx - index position
+        element - the new value
+    """
     copy = my_list[:]
-    if idx < 0 or idx > len(copy):
+    if idx < 0 or idx >= len(copy):
         return copy
     copy[idx] = element
     return copy
