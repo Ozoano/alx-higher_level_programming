b66ebb0b8654ae12c822f57d29e5bf0ab0c34237
git Ozoano <git obiuchenna74@gmail.com>
2022-09-25 08:10:05 -0700

    relace ilist

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -8,6 +8,6 @@ def replace_in_list(my_list, idx, element):
         element - the new value
     """
     if idx < 0 or idx >= len(my_list):
-        returny_list
+        return my_list
     my_list[idx] = element
     return my_list
