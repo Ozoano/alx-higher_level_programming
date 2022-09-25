602a2b96a1cf40cbc188d2c9abff381db2257aa8
git Ozoano <git obiuchenna74@gmail.com>
2022-09-24 23:59:46 -0700

    character string

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """
+    returns a tuple with string length and first character
+    Args:
+        sentence - a string
+    Returns:
+        (lenght, first character)
+    """
+    if not sentence:
+        return None
+    return len(sentence), sentence[0]
+
+
+if __name__ == '__main__':
+    l, f = multiple_returns("At School, I learnt C!")
+print("Length: {:d} - First character: {}".format(l, f))
