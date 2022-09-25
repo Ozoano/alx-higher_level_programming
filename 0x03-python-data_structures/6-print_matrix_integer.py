9fac88213c5706d19a994e07ccf596c338699cc1
git Ozoano <git obiuchenna74@gmail.com>
2022-09-25 08:56:16 -0700

    list matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-	"""
-	prints a matrix of integers.
-	Args:
-		matrix - list matrix
-	Return:
-		matrix integers
-	"""
-	for row in matrix:
-            for val in row:
-                print("{}".format(val), end=" ")
-            print()
+    """
+    prints a matrix of integers.
+    Args:
+    matrix - list matrix
+    Return:
+        matrix integers
+    """
+    for row in matrix:
+        print(" ".join("{:d}".format(j) for j in row))
