7d78f98e9498a98e0add604729f80e901ee6bfaf
git Ozoano <git obiuchenna74@gmail.com>
2022-09-28 16:21:47 -0700

    dict muktiply

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,4 +2,4 @@
 def multiply_by_2(a_dict):
     if a_dict is None:
         return None
-    return map(lambda k: a_dict[k] * 2 for k in a_dict))
+    return {map(lambda k: a_dict[k] * 2 for k in a_dict)}
