cdc7ead081cbef147d084d45805316837573f07e
git Ozoano <git obiuchenna74@gmail.com>
2022-09-28 13:15:41 -0700

    dict multiply

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dict):
-    mult = {}
-    copy_dict = a_dict[:]
-
     if copy_dict is None:
         return None
-    for k, v in copy_dict.items():
-        for item in value:
-            mult.append(v * 2)
-        return copy_dict
+    for k in a_dict:
+        a_dict[k] * 2
+    return a_dict
