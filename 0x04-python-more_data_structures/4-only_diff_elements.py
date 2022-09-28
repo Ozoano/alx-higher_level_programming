b8b941a25a50c16d992b1af00d6f504b3af4e939
git Ozoano <git obiuchenna74@gmail.com>
2022-09-28 10:16:18 -0700

    set Symmetric

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    if (set_1 ^ set_2):
+        return (set_1 ^ set_2)
+    else:
+        return ("set asymetric")
+
+
+if __name__ == '__main__':
+
+    set_1 = {"Python", "C", "Javascript"}
+    set_2 = {"Bash", "C", "Ruby", "Perl"}
+    od_set = only_diff_elements(set_1, set_2)
+    print(sorted(list(od_set)))
