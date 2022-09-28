a733a0a6d8a1440a9e2275602fd2813f32d511c9
git Ozoano <git obiuchenna74@gmail.com>
2022-09-28 16:32:57 -0700

    set symmetric

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
+    if (set_1 ^ set_2) is None:
+        return None
     if (set_1 ^ set_2):
         return (set_1 ^ set_2)
-    else:
-        return("set asymetric")
 
 
 if __name__ == '__main__':
