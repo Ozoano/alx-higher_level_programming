b3135b95ae43f1b904d3a5d538e25d72dbfea4e9
git Ozoano <git obiuchenna74@gmail.com>
2022-09-28 18:10:40 -0700

    tuple average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is None or my_list == {}:
+        return None
+    total = sum(map(lambda x: x[0] * x[1], my_list))
+    num = sum(map(lambda x: x[1], my_list))
+    ave = total / num
+    return ave
+
+
+if __name__ == '__main__':
+    my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
+    # = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
+    result = weight_average(my_list)
+    print("Average: {:0.2f}".format(result))
