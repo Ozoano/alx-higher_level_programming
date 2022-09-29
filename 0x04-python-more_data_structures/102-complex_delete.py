1e57874a5521cdd21bdce5a455b0e8456e3426bb
git Ozoano <git obiuchenna74@gmail.com>
2022-09-28 19:20:50 -0700

    complex delete

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
 def complex_delete(a_dict, val):
-    for k, v in a_dict.items():
-        if a_dict[v] == val
-        a_dict.pop(k)
+    if a_dict is None:
+        return None
+
+    for k in a_dict:
+        if k == val:
+            del a_dict[k]
+    return a_dict
+
+if __name__ == '__main__':
+    a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
+    new_dict = complex_delete(a_dictionary, 'C')
+    print(a_dictionary)
+    print("--")
+    print(new_dict)
+
+    print("--")
+    print("--")
+    new_dict = complex_delete(a_dictionary, 'c_is_fun')
+    print(a_dictionary)
+    print("--")
+    print(new_dict)
