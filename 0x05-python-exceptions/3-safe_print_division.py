041c7b9b0fdcfab7021fb117ae95490cf0aecdb2
git Ozoano <git obiuchenna74@gmail.com>
2022-10-10 07:53:51 -0700

    try_except

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,6 @@ def safe_print_division(a, b):
         res = a / b
     except ZeroDivisionError:
         res = None
-    else:
-        return res
     finally:
         print("Inside result:"{}".format(res))
+    return res
