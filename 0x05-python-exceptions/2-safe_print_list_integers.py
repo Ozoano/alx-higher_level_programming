48553358aa624218a8339bcb100dfe7e2892985e
git Ozoano <git obiuchenna74@gmail.com>
2022-10-10 08:21:13 -0700

    try_except

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     a = 0
-    try:
-        for i in range(x):
-            print(my_list[i], end="")
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
             a += 1
     except (TypeError, ValueError):
         pass
