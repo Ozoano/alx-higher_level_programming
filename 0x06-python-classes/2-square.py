78ad3aec0fbfe81b456d2fff26eae169b36af6ca
git Ozoano <git obiuchenna74@gmail.com>
2022-10-11 08:07:26 -0700

    add a new sqaure

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" square class """
+
+
+class Square:
+    """ square method """
+
+    def __init__(self, size=0):
+        """
+        initialize a new square
+        Args:
+            size(int): size of the new square
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
