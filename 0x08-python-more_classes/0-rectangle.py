b50ddc90ebc855c07f2b495f4503c4adf4b42e0f
git Ozoano <git obiuchenna74@gmail.com>
2022-10-17 11:01:15 -0700

    getter/setter for width/height

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 """defines class Rectangle"""
 class Rectangle:
     """empty class"""
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""defines a class Rectangle"""
+
+
+class Rectangle:
+
+    @property
+    def width(self):
+        """getter for the private instance attribute width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """getter for the private instance attribute width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter for the private instance attribute height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """getter for the private instance attribute height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __init__(self, width=0, height=0):
+        """Representation of a rectangle"""
+        self.width = width
+        self.height = height
