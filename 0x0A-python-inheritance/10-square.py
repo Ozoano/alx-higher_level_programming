4fe151c073dcb7fdcf7c1f58bdcf7af0cc68870f
git Ozoano <git obiuchenna74@gmail.com>
2022-10-24 17:38:36 -0700

    inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,17 +1,19 @@
-#!/usr/bin/python
-"""defines class Rectangle"""
+#!/usr/bin/python3
+"""
+Contains the class BaseGeometry and subclass Rectangle
+"""
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """represents a Rectangle"""
+    """A representation of a square"""
     def __init__(self, size):
-        """instanciation of a Rectangle"""
+        """instantiation of the square"""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        """returnns the area of a square"""
-        return (self.__size) ** 2
+        """"returns the area of the square"""
+        return self.__size ** 2
--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """defines class BaseGeometry"""
 
+
 class BaseGeometry:
+    """represents a class"""
     def area(self):
         """raises exception error"""
         raise Exception("area() is not implemented")
