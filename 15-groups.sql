f75a0668c918776ad080a3a7e149007f75668cd8
git Ozoano <git obiuchenna74@gmail.com>
2022-11-30 17:34:59 -0800

    Pushing task 15 & 16

--- 15-groups.sql
+++ 15-groups.sql
@@ -1,6 +1,5 @@
 -- a script that lists the number of records with the same score
-SELECT score,
-COUNT(score) AS number
-FROM second_table
-GROUP BY score
-ORDER BY number DESC;
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
