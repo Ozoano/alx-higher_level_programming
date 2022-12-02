072369d3afd30d2a84638340cc59b2e630025b00
git Ozoano <git obiuchenna74@gmail.com>
2022-12-01 20:46:42 -0800

    more queries

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- a script that creates database and a user
+-- user should have only SELECT privilege
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
